43df1511f43448433feb4373b76a3cbaa2a485e5
Farhad Alishov <farhad.alishov@ucalgary.ca>
2021-03-27 22:43:38 +0400

    Suggestions

--- hyl_project/src/hyl_project/ProjectMain.java
+++ hyl_project/src/hyl_project/ProjectMain.java
@@ -5,6 +5,22 @@ public class ProjectMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TESTING NOW
+
+		// args = String  furnitureCategory, String furnitureType, and int requestedAmount
+	}
+
+	public class calculateOption(){
+
+		if (requestPossible){
+			produceOutput();
+		} else {
+			printSuggestions();
+		}
+
+	}
+
+	public class produceOutput(){
+		
 	}
 
 }
