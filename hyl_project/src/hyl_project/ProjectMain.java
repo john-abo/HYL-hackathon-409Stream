2a76f4c8b145fe582f1592d42d022610c8ad0a03
Farhad Alishov <farhad.alishov@ucalgary.ca>
2021-03-28 02:00:25 +0400

    Create ProjectMain.java

---
+++ hyl_project/src/hyl_project/ProjectMain.java
@@ -0,0 +1,40 @@
+package hyl_project;
+
+public class ProjectMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+<<<<<<< HEAD
+		// TESTING NOW
+
+		// args = String  furnitureCategory, String furnitureType, and int requestedAmount
+	}
+
+	public class calculateOption(){
+
+		if (requestPossible){
+			produceOutput();
+		} else {
+			printSuggestions();
+		}
+
+	}
+
+	public class produceOutput(){
+		
+=======
+		//TEST
+		FormPrinter printer;
+		
+		System.out.println("TEST");
+		
+		//Can't seem to get REGEX to find it without end of string having a space
+		printer = new FormPrinter("mesh chair, 1 ");
+		
+		System.out.println(printer.getType() + ", " + printer.getFurniture() + ", " + printer.getQuantity());
+		
+		printer.writeReport();	//Writes report
+>>>>>>> HYL-project
+	}
+
+}
