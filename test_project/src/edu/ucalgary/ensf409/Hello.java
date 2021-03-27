eda7b88d3ddc04abbeea41f8800615c533257c57
john-abo <john.abo@ucalgary.ca>
2021-03-27 10:58:26 -0600

    Revert "Attempting to fix something"
    This reverts commit b72431fda819a1e40c872bd6eef0e850e55b0f06.

---
+++ test_project/src/edu/ucalgary/ensf409/Hello.java
@@ -0,0 +1,20 @@
+package edu.ucalgary.ensf409;
+
+public class Hello {
+	/**
+	 * Make some edits here
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		int x = 5;
+		System.out.println("Hello, good morning");
+	}
+	
+	//Some changes
+}
