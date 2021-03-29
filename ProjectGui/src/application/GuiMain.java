1598dcaafe6c771f37fe3271785636ae870330fd
john-abo <john.abo@ucalgary.ca>
2021-03-28 22:27:13 -0600

    Removed unused method
    The method was used for printing to console. now that there's a GUI it's not needed.

--- ProjectGui/src/application/GuiMain.java
+++ ProjectGui/src/application/GuiMain.java
@@ -22,41 +22,12 @@ public class GuiMain {
 	
 	private Label requestNotif;
 	private static String userIn = "";
-	
-	/**
-	 * Gets the user input. The message printed is meant to notifty the user about the format
-	 * and the meaning of each input. This is run in console
-	 */
-	@SuppressWarnings("unused") //Suppressed for now, as I am not using it
-	private static void userInputConsole() {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter request in the format");
-		System.out.println("\n<furniture type> <furniture>, <quantity>\n");
-		System.out.println("<Furniture type>:	Type of furniture, such as mesh, Adjustable, or Swing Arm");
-		System.out.println("<Furniture>:		Furniture that's being checked, such as Desk, Lamp, Chair");
-		System.out.println("<Quantity>:			Amount of specific furniture you are looking for");
-		System.out.println("\nEnter:");
-		
-		userIn = input.nextLine();
-		
-		input.close();
-	}
 
 	/**
 	 * Launch the application.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//FormPrinter printer;
-		
-		//userInputConsole();
-		//System.out.println("User request: " + userIn);
-		
-		//printer = new FormPrinter(userIn);
-		//System.out.println(printer.getType() + ", " + printer.getFurniture() + ", " + printer.getQuantity());
-		
-		//printer.writeReport();	//Writes report
 		
 		try {
 			GuiMain window = new GuiMain();
