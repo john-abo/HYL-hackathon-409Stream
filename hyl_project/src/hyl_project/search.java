b8963cdab88fdd18671372ef2ac523b85b5163ba
KHasan8 <kylehasan1@gmail.com>
2021-03-30 14:07:04 -0600

    orders multiple items

--- hyl_project/src/hyl_project/search.java
+++ hyl_project/src/hyl_project/search.java
@@ -9,14 +9,21 @@ public class search {
 	private ResultSet results;
 	//done contains the power set
 	private ArrayList<ArrayList<node>> done = new ArrayList<ArrayList<node>>();
-
+	/**
+	 * 
+	 * @param dBURL url of database
+	 * @param uSERNAME username of user
+	 * @param pASSWORD password of user
+	 */
 	public search( String dBURL, String uSERNAME, String pASSWORD) {
 		
 		DBURL = dBURL;
 		USERNAME = uSERNAME;
 		PASSWORD = pASSWORD;
 	}
-
+	/**
+	 * connects to sql database
+	 */
 	public void initializeConnection() {
 		 try {
 			dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
@@ -27,18 +34,25 @@ public class search {
 		
 	}
 	
-	public ArrayList<String> searchFiling(String lookup){
+	/**
+	 * 
+	 * @param lookup type of filing
+	 * @param needed number needed for order
+	 * @return arraylist string containing ids of all ordered items, returns null if order not possible
+	 */
+	public ArrayList<String> searchFiling(String lookup,int needed){
 		//contains the combination with the lowest price. Price is always at the END of the arraylist. If combination cannot be created, this is empty
-    	ArrayList<String> done2 = new ArrayList<String>();
-    	int index1 = -1;
+	    ArrayList<String> done2 = new ArrayList<String>();
+	    int orderPrice = 0;
+	    int combinedPrice = 0;
     	try {
+    		
     		//finds all entries of chair with the specified type and saves them into results
     		PreparedStatement find = dbConnect.prepareStatement("SELECT * FROM filing WHERE Type=?");
 			find.setString(1,lookup);
 			results = find.executeQuery();
 			//current price to save
-			int price = 2147483647;
-			int combinedPrice = 0;
+			
 			//used to check if combination is valid
 			boolean Rails = false;
 			boolean Cabinet = false;
@@ -52,7 +66,7 @@ public class search {
 			while (results.next()){
 				if(results.getString("Type").equals(lookup)) {
 					
-				   filingData temp = new filingData(results.getString("ID"),results.getString("Legs"),results.getString("Rails"),results.getString("filing"),results.getInt("Price"));
+				   filingData temp = new filingData(results.getString("ID"),results.getString("Rails"),results.getString("Cabinet"),results.getString("Drawers"),results.getInt("Price"));
 				    temp1 = new node(temp);
 				   matches.add(temp1);
 				 
@@ -64,6 +78,13 @@ public class search {
 			if(temp1 == null) {
 				return null;
 			}
+			//iterates once to find each item required in order
+			for(int b = 0; b < needed ; b++) {
+				int price = 2147483647;
+				
+				done = new ArrayList<ArrayList<node>>();
+			
+		    int index1 = -1;
 			//empty arraylist
 			ArrayList<node> g = new ArrayList<node>();
 			//finds the power set for the matches list
@@ -73,6 +94,7 @@ public class search {
 			done.remove(0);
 			//iterates over each subset in power set
 	    	for(int i = 0; i < done.size(); i++) {
+	    		combinedPrice = 0;
 	    		//iterates over each element in subset
 	    		for(int j = 0; j< done.get(i).size(); j++) {
 	    			filingData v = (filingData) done.get(i).get(j).element;
@@ -95,29 +117,36 @@ public class search {
 	    		//System.out.println(combinedPrice);
 	    		//if combination is valid and whether the combined price of the combination is smallest possible one
 	    		if(combinedPrice < price && total) {
-	    			
+	    			index1 = i;
+	    			//System.out.println(combinedPrice);
 	    			price = combinedPrice;
 	    		}
 	    		//System.out.println();
 	    		//resets variables for each iteration
-	    		combinedPrice = 0;
+	    		
 	    		Rails = false;
 	    		Cabinet = false;
 	    		
 	    		Drawers = false;
 	    	}
 			
-	    	System.out.println(price);
+	    	//System.out.println(price);
+	    	//if no valid combo is found, index1 is -1. Therefore order is invalid
 	      	if(index1 ==-1) {
+	      		
 	    		return null;
 	    	}
 	    	ArrayList<node> finalCombo = done.get(index1);
 	    	for(int h = 0; h < finalCombo.size(); h++) {
 	    		done2.add(finalCombo.get(h).id);
+	    		//removes currently found combination from list of available items
+	    		matches.remove(finalCombo.get(h));
 	    		
 	    	}
-	    	done2.add(String.valueOf(price));
-	    	done.clear();
+	    	
+	    	orderPrice += price;
+	    	
+			}
 			//go.printList();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -125,14 +154,23 @@ public class search {
 		}
     	
     	
+		done2.add("$" + String.valueOf(orderPrice));
 		return done2;
 		
 		
 	}
-	public ArrayList<String> searchLamp(String lookup){
+	/**
+	 * 
+	 * @param lookup type of lamp needed in order
+	 * @param needed number of lamps needed in order
+	 * @return arraylist containing ordered lamps, with price always at the end. Returns null for invalid orders
+	 */
+	public ArrayList<String> searchLamp(String lookup,int needed){
+		
 		//contains the combination with the lowest price. Price is always at the END of the arraylist. If combination cannot be created, this is empty
     	ArrayList<String> done2 = new ArrayList<String>();
-    	int index1 = -1;
+    	//price of order
+    	int orderPrice = 0;
     	try {
     		//finds all entries of chair with the specified type and saves them into results
     		PreparedStatement find = dbConnect.prepareStatement("SELECT * FROM LAMP WHERE Type=?");
@@ -140,7 +178,7 @@ public class search {
 			results = find.executeQuery();
 			//current price to save
 			int price = 2147483647;
-			int combinedPrice = 0;
+			
 			//used to check if combination is valid
 			boolean Base = false;
 			boolean Bulb = false;
@@ -166,6 +204,12 @@ public class search {
 			if(temp1 == null) {
 				return null;
 			}
+		
+			//iterates once for each item required in order
+			for(int b = 0; b < needed ;b++) {
+			done = new ArrayList<ArrayList<node>>();
+			int index1 = -1;
+			int combinedPrice = 0;
 			//empty arraylist
 			ArrayList<node> g = new ArrayList<node>();
 			//finds the power set for the matches list
@@ -197,7 +241,7 @@ public class search {
 	    		//System.out.println(combinedPrice);
 	    		//if combination is valid and whether the combined price of the combination is smallest possible one
 	    		if(combinedPrice < price && total) {
-	    			
+	    			index1 = i;
 	    			price = combinedPrice;
 	    		}
 	    		//System.out.println();
@@ -209,30 +253,42 @@ public class search {
 	    	
 	    	}
 			
-	    	System.out.println(price);
+	    	//System.out.println(price);
+	    	//if valid combo could not be found index = -1. Therefore order is invalid
 	      	if(index1 ==-1) {
 	    		return null;
 	    	}
 	    	ArrayList<node> finalCombo = done.get(index1);
 	    	for(int h = 0; h < finalCombo.size(); h++) {
-	    		done2.add(finalCombo.get(h).id);
 	    		
+	    		done2.add(finalCombo.get(h).id);
+	    		//removes currently found combo items from available items
+	    		matches.remove(finalCombo.get(h));
+	    		//System.out.println(done2.get(h));
 	    	}
-	    	done2.add(String.valueOf(price));
-	    	done.clear();
+	    	//increases price of order by currently found combo price
+	    	orderPrice += price;
+			}
 			//go.printList();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
+    	done2.add("$" + String.valueOf(orderPrice));
     	
 		return done2;
 	}
-	public ArrayList<String> searchDesk(String lookup){
+	/**
+	 * orders desks
+	 * @param lookup type of desk needed for order
+	 * @param needed number of desks needed for order
+	 * @return arraylist containg ids of desks order.Returns null for invalid order. Price always at the end as string.
+	 */
+	public ArrayList<String> searchDesk(String lookup, int needed){
+		int orderPrice = 0;
 		//contains the combination with the lowest price. Price is always at the END of the arraylist. If combination cannot be created, this is empty
     	ArrayList<String> done2 = new ArrayList<String>();
-    	int index1 = -1;
+    
     	try {
     		//finds all entries of chair with the specified type and saves them into results
     		PreparedStatement find = dbConnect.prepareStatement("SELECT * FROM DESK WHERE Type=?");
@@ -240,7 +296,7 @@ public class search {
 			results = find.executeQuery();
 			//current price to save
 			int price = 2147483647;
-			int combinedPrice = 0;
+			
 			//used to check if combination is valid
 			boolean Top = false;
 			boolean legs = false;
@@ -254,8 +310,8 @@ public class search {
 			while (results.next()){
 				if(results.getString("Type").equals(lookup)) {
 					
-				   deskData temp = new deskData(results.getString("ID"),results.getString("Legs"),results.getString("Top"),results.getString("Desk"),results.getInt("Price"));
-				    temp1 = new node(temp);
+				   deskData temp = new deskData(results.getString("ID"),results.getString("Legs"),results.getString("Top"),results.getString("Drawer"),results.getInt("Price"));
+				    temp1 = new node<deskData>(temp);
 				   matches.add(temp1);
 				 
 				   
@@ -264,9 +320,14 @@ public class search {
 	            }
 			//returns immediately if no results were found
 			if(temp1 == null) {
+				
 				return null;
 			}
 			//empty arraylist
+			for(int b= 0 ; b < needed ; b++) {
+			int combinedPrice = 0;
+			done = new ArrayList<ArrayList<node>>();
+			int index1 = -1;
 			ArrayList<node> g = new ArrayList<node>();
 			//finds the power set for the matches list
 
@@ -275,7 +336,7 @@ public class search {
 			done.remove(0);
 			//iterates over each subset in power set
 	    	for(int i = 0; i < done.size(); i++) {
-	    		//iterates over each element in subset
+	    		
 	    		for(int j = 0; j< done.get(i).size(); j++) {
 	    			deskData v = (deskData) done.get(i).get(j).element;
 	    			//checks if combination is valid by checking if at least 1 element is true for each part
@@ -296,8 +357,8 @@ public class search {
 	    		}
 	    		//System.out.println(combinedPrice);
 	    		//if combination is valid and whether the combined price of the combination is smallest possible one
-	    		if(combinedPrice < price && total) {
-	    			
+	    		if(combinedPrice <= price && total) {
+	    			index1 = i;
 	    			price = combinedPrice;
 	    		}
 	    		//System.out.println();
@@ -309,33 +370,42 @@ public class search {
 	    		Drawer = false;
 	    	}
 			
-	    	System.out.println(price);
+	    	//System.out.println(price);
 	      	if(index1 ==-1) {
+	      		
 	    		return null;
 	    	}
 	    	ArrayList<node> finalCombo = done.get(index1);
 	    	for(int h = 0; h < finalCombo.size(); h++) {
-	    		done2.add(finalCombo.get(h).id);
 	    		
+	    		done2.add(finalCombo.get(h).id);
+	    		matches.remove(finalCombo.get(h));
 	    	}
-	    	done2.add(String.valueOf(price));
-	    	done.clear();
+	    	//increases price of order by currently found combo price
+	    	orderPrice += price;
+			}
 			//go.printList();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	
-    	
+    	done2.add("$"+String.valueOf(orderPrice));
 		return done2;
 		
 	}
-	public ArrayList<String> searchChair(String lookup) {
+	/**
+	 * finds chair order
+	 * @param lookup type of chair
+	 * @param needed amount needed for order
+	 * @return arraylist of ids ordered. String containg total price is always at the end.If order is invalid, null is returned.
+	 */
+	public ArrayList<String> searchChair(String lookup,int needed) {
+		int orderPrice = 0;
 		
-    
     	//contains the combination with the lowest price. Price is always at the END of the arraylist. If combination cannot be created, this is empty
     	ArrayList<String> done2 = new ArrayList<String>();
-    	int index1 = -1;
+    	
     	
     	try {
     		//finds all entries of chair with the specified type and saves them into results
@@ -344,7 +414,7 @@ public class search {
 			results = find.executeQuery();
 			//current price to save
 			int price = 2147483647;
-			int combinedPrice = 0;
+		
 			//used to check if combination is valid
 			boolean arm = false;
 			boolean legs = false;
@@ -370,6 +440,11 @@ public class search {
 			if(temp1 == null) {
 				return null;
 			}
+			//iterates once for each item required in order
+			for(int b = 0; b < needed ; b++) {
+			int index1 = -1;
+			done = new ArrayList<ArrayList<node>>();
+			int combinedPrice = 0;
 			//empty arraylist
 			ArrayList<node> g = new ArrayList<node>();
 			//finds the power set for the matches list
@@ -414,23 +489,27 @@ public class search {
 	    		legs = false;
 	    	}
 			
-	    	System.out.println(price);
+	    	//System.out.println(price);
+	    	//if index1 = -1, then no valid combo could be found and therefore order is invalid
 	    	if(index1 ==-1) {
 	    		return null;
 	    	}
 	    	ArrayList<node> finalCombo = done.get(index1);
 	    	for(int h = 0; h < finalCombo.size(); h++) {
 	    		done2.add(finalCombo.get(h).id);
+	    		//removes currently found combo from list of availiable items
+	    		matches.remove(finalCombo.get(h));
 	    		
 	    	}
-	    	done2.add(String.valueOf(price));
-	    	done.clear();
+	    	//increases price of order by currently found combo price
+	    	orderPrice += price;
+			}
 			//go.printList();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
+    	done2.add("$"+String.valueOf(orderPrice));
 		return done2;
 		
 		
